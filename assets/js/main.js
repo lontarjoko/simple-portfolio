ec78e1a821dfbb50ea8840c62cf7f61b99681a90
Wachid Amirul <wachidamirul@gmail.com>
2022-07-23 08:46:02 +0700

    finish section testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,25 @@ linkWork.forEach(l => l.addEventListener('click', activeWork))
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
-
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
+  autoplay: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 48,
+    }
+  },
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
